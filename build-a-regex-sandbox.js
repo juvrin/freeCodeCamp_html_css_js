05cdc5cd2ad6535eb042151ddad3f23f33994363
Jules <195559278+spacecookie007@users.noreply.github.com>
2026-01-02 16:23:12 +0100

    Single match function is ok, multiple matches still to do

--- build-a-regex-sandbox.js
+++ build-a-regex-sandbox.js
@@ -12,19 +12,35 @@ function getFlags(){
   return outArr.join("")
 }
 
+
 function regexSandbox(regexInput, str){
   const regex = new RegExp(`${regexInput}`, `${getFlags()}`);
-  console.log(str.match(regex));
+  // console.log(str);
+  
   // console.log(str.match(regex)[0].length);
-  if (str.match(regex)[0] !== "") {
-    let startIndex = str.match(regex).index;
-    let endIndex = startIndex + str.match(regex)[0].length - 1;
-    stringToTest.innerHTML = `<span class="highlight></span>` 
+  const matchArray = str.match(regex);
 
-    //highlight from index to length of match
+  //check for matches
+  if (matchArray !== null) {
+
+    //check if only one match
+    if(globalFlag.checked == false){
+    const startString = str.slice(0,matchArray.index);
+    const endString = str.slice(matchArray.index + matchArray[0].length,);
+    stringToTest.innerHTML = `${startString}<span class="highlight">${matchArray[0]}</span>${endString}`;
+    testResult.textContent = `${str}`
+    // console.log("just one match");
+    } else{
+    console.log(matchArray);
+    console.log("multiple matches");
+    }
+    
+  }else{
+    testResult.textContent = "no match";
   }
   
 }
+
 
 
 testButton.addEventListener("click",() => regexSandbox(regexPattern.value, stringToTest.textContent));
