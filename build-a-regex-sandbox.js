4cf58194d856f3f4ed9f3ac66c4ed22f25dbcb4f
Jules <195559278+spacecookie007@users.noreply.github.com>
2026-01-03 14:41:46 +0100

    More elegant solution with function als second argument of replace, much shorter code

--- build-a-regex-sandbox.js
+++ build-a-regex-sandbox.js
@@ -13,36 +13,12 @@ function getFlags(){
 }
 
 function regexSandbox(regexInput, str){
-  const regex = new RegExp(`${regexInput}`, `${getFlags()}`);
-  const matchArray = str.match(regex);
-  const spanStart = '<span class="highlight">';
-  const spanEnd = '</span>';
-
-  //check for matches
-  if (matchArray !== null) {
-
-    //check if only one match
-    if(globalFlag.checked == false){
-    const startString = str.slice(0, matchArray.index);
-    const endString = str.slice(matchArray.index + matchArray[0].length,);
-    stringToTest.innerHTML = `${startString}${spanStart}${matchArray[0]}${spanEnd}${endString}`;
-    testResult.textContent = `${matchArray[0]}`
-    } else{
-      const matchArr = Array.from(str.matchAll(regex));
-      const notMatch = str.split(regex);
-      let resultArr = [];
-      let highlightRes = [];
-      highlightRes.push(notMatch[0]);
-   
-      for(let i = 0; i < matchArr.length; i++){
-        resultArr.push(matchArr[i][0]);
-        highlightRes.push(`${spanStart}${matchArr[i][0]}${spanEnd}`);
-        highlightRes.push(notMatch[i+1]);
-      };
-      testResult.textContent = `${resultArr.join(", ")}`
-      stringToTest.innerHTML = highlightRes.join("");       
-    };
-    
+  const regex = new RegExp(regexInput, getFlags());
+  const matches = str.match(regex);
+ 
+  if (matches) {
+    testResult.textContent = matches.join(", ")
+    stringToTest.innerHTML = stringToTest.innerHTML.replace(regex, (content) => `<span class='highlight'>${content}</span>`)
   }else{
     testResult.textContent = "no match";
   };
