c88e8406fb56c0b25d8303de4adb12045b4b50a9
Jules <195559278+spacecookie007@users.noreply.github.com>
2026-01-03 11:39:30 +0100

    Solution

--- build-a-regex-sandbox.js
+++ build-a-regex-sandbox.js
@@ -12,42 +12,40 @@ function getFlags(){
   return outArr.join("")
 }
 
-
 function regexSandbox(regexInput, str){
   const regex = new RegExp(`${regexInput}`, `${getFlags()}`);
   const matchArray = str.match(regex);
-  
+  const spanStart = '<span class="highlight">';
+  const spanEnd = '</span>';
+
   //check for matches
   if (matchArray !== null) {
 
     //check if only one match
     if(globalFlag.checked == false){
-    const startString = str.slice(0,matchArray.index);
+    const startString = str.slice(0, matchArray.index);
     const endString = str.slice(matchArray.index + matchArray[0].length,);
-    stringToTest.innerHTML = `${startString}<span class="highlight">${matchArray[0]}</span>${endString}`;
+    stringToTest.innerHTML = `${startString}${spanStart}${matchArray[0]}${spanEnd}${endString}`;
     testResult.textContent = `${matchArray[0]}`
     } else{
-      console.log("multiple matches");
       const matchArr = Array.from(str.matchAll(regex));
-    console.log(matchArr);
-    // for(let match of matchArray){
-    //   console.log(match);
-    // }
-    
-
-    for(let match of matchArr){
-      console.log(match.index);
-      //hier kan je dan pushen naar een outArr(die je later kan joinen zodat string met komma's ertussen) door steeds de str vanaf 0 tot 1e match index, dan str vanaf 1e match index+match.length tot aan 2e match index etc
-    }
-
-
-    
+      const notMatch = str.split(regex);
+      let resultArr = [];
+      let highlightRes = [];
+      highlightRes.push(notMatch[0]);
+   
+      for(let i = 0; i < matchArr.length; i++){
+        resultArr.push(matchArr[i][0]);
+        highlightRes.push(`${spanStart}${matchArr[i][0]}${spanEnd}`);
+        highlightRes.push(notMatch[i+1]);
+      };
+      testResult.textContent = `${resultArr.join(", ")}`
+      stringToTest.innerHTML = highlightRes.join("");       
     };
     
   }else{
     testResult.textContent = "no match";
   };
-  
 };
 
 
