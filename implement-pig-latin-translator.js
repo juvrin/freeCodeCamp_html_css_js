a001d97bd335a8cdc1a7929be4aecf3a37a77dc8
Jules <195559278+spacecookie007@users.noreply.github.com>
2026-01-06 11:04:58 +0100

    Solution

--- implement-pig-latin-translator.js
+++ implement-pig-latin-translator.js
@@ -1,15 +1,22 @@
 function translatePigLatin(str){
-  const vowels = /^(A|E|I|O|U)/i;
-  const consonants = /[b-df-hj-np-tv-z]*(A|E|I|O|U)*/gi;
+  const vowels = /^(a|e|i|o|u)/i;
+const consonants = /^([b-df-hj-np-tv-z])*/i;
+
 
   if(vowels.test(str)){
     return str + "way";
-  }else{
-    console.log(str.match(consonants))
+  }else {
+    let startCons = str.match(consonants)[0].toLowerCase();
+    let startIndex = str.match(consonants).index + startCons.length;
+    console.log(startIndex);
+    return str.slice(startIndex,) + startCons + "ay";
+    
   };
 };
 
-let test = translatePigLatin("U are doing you're best");
-let test2 = translatePigLatin("How are you")
+// let test = translatePigLatin("U are doing you're best");
 // console.log(test);
+
+let test2 = translatePigLatin("Shows are on")
 console.log(test2);
+
