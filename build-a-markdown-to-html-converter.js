0adcb77610e4fe62fa551a1dc666095bcd6d834c
Jules <195559278+spacecookie007@users.noreply.github.com>
2026-01-08 15:37:04 +0100

    All converters ok but not yet loop

--- build-a-markdown-to-html-converter.js
+++ build-a-markdown-to-html-converter.js
@@ -5,29 +5,61 @@
 // markdownInput.addEventListener("input",() => console.log(markdownInput.value));
 
 //HEADINGS
-const regexHeadings = /^(?<level>#+)\s(?<rest>[a-zA-Z|\s]+)/;
+const regexHeadings = /^\s*(?<level>#+)\s(?<rest>[a-zA-Z|\s]+)/;
 let testHeadings = "### DOes this work"
-let newHead = testHeadings.replace(regexHeadings, `<h${testHeadings.match(regexHeadings)[1].length}>$<rest></h${testHeadings.match(regexHeadings)[1].length}>`);
+let replaceHeadings = `<h${testHeadings.match(regexHeadings)[1].length}>$<rest></h${testHeadings.match(regexHeadings)[1].length}>`
+let newHead = testHeadings.replace(regexHeadings, replaceHeadings);
 // console.log(newHead);
-
-//DIT WERKT NOG NIET
-// let newText2 = testText.replace(regexHeadings, (content) => 
-//     {   let headNum = testText.match(regexHeadings)[1].length;
-//         console.log(content);
-//         return `<h${headNum}>$<rest></h${headNum}>`});
-// console.log(newText2);
-
 
 //BOLD
 const regexBold = /^(?<bold>\*{2}|_{2})(?<text>.+)\k<bold>/;
+let replaceBold = "<strong>$<text></strong>";
 let testBold1 = "**Does this work**";
 let testBold2 = "__Does this work__";
-
-console.log(regexBold.test(testBold1));
-console.log(regexBold.test(testBold2));
-// console.log(regexBoldSimple.test(testBold1));
-
-let newBold1 = testBold1.replace(regexBold,"<strong>$<text></strong>")
+let newBold1 = testBold1.replace(regexBold,replaceBold)
 let newBold2 = testBold2.replace(regexBold,"<strong>$<text></strong>")
-console.log(newBold1);
-console.log(newBold2);
+
+//ITALIC
+const regexItalic = /^(?<bold>\*{1}|_{1})(?<text>.+)\k<bold>/;
+let replaceItalic = "<em>$<text></em>"
+let testItalic1 = "*Does this work*";
+let testItalic2 = "_Does this work_";
+let newItalic1 = testItalic1.replace(regexItalic, replaceItalic)
+let newItalic2 = testItalic2.replace(regexItalic,"<em>$<text></em>")
+// console.log(newItalic1);
+
+//IMG
+const regexImg = /^\!\[(?<alt>.*)\]\((?<src>.*)\)/;
+let replaceImg = "<img alt='$<alt>' src='$<src>'>"
+let testImg = "![alt text hier](https://www.nogiets.com/imgcute/)";
+let newImg = testImg.replace(regexImg,replaceImg);
+// console.log(newImg);
+
+//LINK
+const regexLink = /^\[(?<linktext>.*)\]\((?<url>.*)\)/;
+let replaceLink = "<a href='$<url>'>$<linktext></a>"
+let testLink = "[click me](www.doggies.com)";
+let newLink = testLink.replace(regexLink,replaceLink);
+console.log(newLink);
+
+//QUOTE
+const regexQuote = /\s*\>\s{1}(?<quotetext>.*)/;
+let replaceQuote = "<blockquote>$<quotetext></blockquote>";
+let testQuote = " > I have a dream that one day bla bla."
+let newQuote = testQuote.replace(regexQuote,replaceQuote);
+console.log(newQuote);
+
+
+//MULTIPLE REGEX
+//je zou dit ook in een dictionary kunnen zetten van regex-replace paren
+// let allRegex = [regexHeadings, regexBold, regexItalic, regexImg];
+// let allReplace = [replaceHeadings, replaceBold, replaceItalic, replaceImg]
+// // let allString = "*Does this work*";
+// let allString = "**Does this work**";
+// for(let i = 0; i < allRegex.length; i++){
+
+//     if(allRegex[i].test(allString)){
+//         console.log(allReplace[i])
+//         return console.log(allString.replace(i, (i) => allReplace[i]))
+//     }
+// };
