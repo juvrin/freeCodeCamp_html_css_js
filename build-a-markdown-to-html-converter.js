dd35aca536e5a09b89bd44fc117e516a2c916fd5
Jules <195559278+spacecookie007@users.noreply.github.com>
2026-01-12 14:22:15 +0100

    Solution

--- build-a-markdown-to-html-converter.js
+++ build-a-markdown-to-html-converter.js
@@ -1,64 +1,27 @@
-//TO DO
-//- linken naar script in html file!
-//- zorgen dat je ze kan combineren bv <h1><strong>some text</strong></h1>
-
-
-
 let markdownInput = document.getElementById("markdown-input");
 let rawHTML = document.getElementById("html-output");
 let preview = document.getElementById("preview");
 
-markdownInput.addEventListener("input",() => convertMarkdown(markdownInput.value));
+
+markdownInput.addEventListener("input",() => {
+  rawHTML.innerText = convertMarkdown();
+  preview.innerHTML = convertMarkdown();
+  });
 
 
-function convertMarkdown(allString){
+
+function convertMarkdown(){
+  let input = markdownInput.value;
   
-  //HEADINGS
-  const regexHeadings = /^\s*(?<level>#+)\s(?<rest>[\w|\s]+)/;
+  let output = input
+    .replace(/(?:^|\n)(###)\s(?<rest>.+)/gm,"<h3>$<rest></h3>")
+    .replace(/(?:^|\n)(##)\s(?<rest>.+)/gm,"<h2>$<rest></h2>")
+    .replace(/(?:^|\n)(#)\s(?<rest>.+)/gm,"<h1>$<rest></h1>")
+    .replace(/(?<bold>\*{2}|_{2})(?<text>.+)\k<bold>\s*/gm,"<strong>$<text></strong>")
+    .replace(/(?<italic>\*{1}|_{1})(?<text>.+)\k<italic>\s*/gm,"<em>$<text></em>")
+    .replace(/\!\[(?<alt>.*)\]\((?<src>.*)\)\s*/gm,"<img alt='$<alt>' src='$<src>'>")
+    .replace(/\[(?<linktext>.*)\]\((?<url>.*)\)\s*/gm,"<a href='$<url>'>$<linktext></a>")
+    .replace(/^\s*\>\s{1}(?<quotetext>.*)\s*/gm,"<blockquote>$<quotetext></blockquote>")
 
-  //BOLD
-  const regexBold = /^(?<bold>\*{2}|_{2})(?<text>.+)\k<bold>/;
-  let replaceBold = "<strong>$<text></strong>";
-
-  //ITALIC
-  const regexItalic = /^(?<italic>\*{1}|_{1})(?<text>.+)\k<italic>/;
-  let replaceItalic = "<em>$<text></em>"
-
-  //IMG
-  const regexImg = /^\!\[(?<alt>.*)\]\((?<src>.*)\)/;
-  let replaceImg = "<img alt='$<alt>' src='$<src>'>"
-
-  //LINK
-  const regexLink = /^\[(?<linktext>.*)\]\((?<url>.*)\)/;
-  let replaceLink = "<a href='$<url>'>$<linktext></a>"
-
-  //QUOTE
-  const regexQuote = /\s*\>\s{1}(?<quotetext>.*)/;
-  let replaceQuote = "<blockquote>$<quotetext></blockquote>";
-
-  //COMBINE MULTIPLE REGEX
-  let allRegex = [regexHeadings, regexBold, regexItalic, regexImg, regexLink, regexQuote];
-  let output = "";
-
-  function replaceHeadingsFunction(str, regex){
-    if(regexHeadings.test(allString)){
-      let level = str.match(regex)[1].length
-      return `<h${level}>$<rest></h${level}>` 
-    };
-    };
-    
-  let allReplace = [replaceHeadingsFunction(allString, regexHeadings), replaceBold, replaceItalic, replaceImg, replaceLink, replaceQuote];
-
-  for(let i = 0; i < allRegex.length; i++){
-      if(allRegex[i].test(allString)){
-          output = allString.replace(allRegex[i], allReplace[i]);
-      }
-  };
-  
-  rawHTML.textContent = output;
-  preview.innerHTML = output;
   return output;
-
 };
-
-
