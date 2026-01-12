100990f5b4471abc2bdfdb5672616ff3bb4727bd
Jules <195559278+spacecookie007@users.noreply.github.com>
2026-01-12 14:26:20 +0100

    Shorter code for replacing headings

--- build-a-markdown-to-html-converter.js
+++ build-a-markdown-to-html-converter.js
@@ -14,9 +14,9 @@ function convertMarkdown(){
   let input = markdownInput.value;
   
   let output = input
-    .replace(/(?:^|\n)(###)\s(?<rest>.+)/gm,"<h3>$<rest></h3>")
-    .replace(/(?:^|\n)(##)\s(?<rest>.+)/gm,"<h2>$<rest></h2>")
-    .replace(/(?:^|\n)(#)\s(?<rest>.+)/gm,"<h1>$<rest></h1>")
+    .replace(/^(###)\s(?<rest>.+)/gm,"<h3>$<rest></h3>")
+    .replace(/^(##)\s(?<rest>.+)/gm,"<h2>$<rest></h2>")
+    .replace(/^(#)\s(?<rest>.+)/gm,"<h1>$<rest></h1>")
     .replace(/(?<bold>\*{2}|_{2})(?<text>.+)\k<bold>\s*/gm,"<strong>$<text></strong>")
     .replace(/(?<italic>\*{1}|_{1})(?<text>.+)\k<italic>\s*/gm,"<em>$<text></em>")
     .replace(/\!\[(?<alt>.*)\]\((?<src>.*)\)\s*/gm,"<img alt='$<alt>' src='$<src>'>")
