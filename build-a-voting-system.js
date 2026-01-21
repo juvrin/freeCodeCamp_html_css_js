a8184157274e74099d8187ff2c9b1c4154043219
Jules <195559278+spacecookie007@users.noreply.github.com>
2026-01-21 11:19:06 +0100

    Solution

--- build-a-voting-system.js
+++ build-a-voting-system.js
@@ -1,14 +1,43 @@
 const poll = new Map();
 
 const addOption = (option) => {
-  if(!option) return "Option cannot be empty"
+  if(!option) return "Option cannot be empty."
   if(!poll.has(option)){
     poll.set(option, new Set());
-    return `Option ${option} added to the poll.`
+    return `Option "${option}" added to the poll.`
   } else if (poll.has(option)){
-    return `Option ${option} already exists.`
+    return `Option "${option}" already exists.`
   }
 };
 
-console.log(addOption("ajskld"));
-console.log(addOption("ajskld"));
+const vote = (option, voterId) => {
+  if(!poll.has(option)){
+    return `Option "${option}" does not exist.`
+  }
+    if(poll.get(option).has(voterId)){
+    return `Voter ${voterId} has already voted for "${option}".`
+  }else{
+    poll.get(option).add(voterId);
+    return `Voter ${voterId} voted for "${option}".`
+  } 
+}
+
+const displayResults = () => {
+  let outString = "Poll Results:\n"
+  poll.forEach((val,key) => {
+    outString += `${key}: ${val.size} votes\n`});
+  let output = outString.substring(0, outString.length - 1);
+  return output;
+}
+
+
+addOption("Turkey");
+addOption("Morocco");
+addOption("Spain");
+
+vote("Turkey","121");
+vote("Morocco","120");
+vote("Turkey","123");
+console.log(displayResults());
+
+
