32e8bba19af5bf001241446e2292b2e78cc3ad98
Jules <195559278+spacecookie007@users.noreply.github.com>
2026-01-22 23:41:42 +0100

    Still some things to do

--- build-a-bookmark-manager-app.js
+++ build-a-bookmark-manager-app.js
@@ -4,18 +4,67 @@ const formSection = document.getElementById("form-section");
 const bookmarkListSection = document.getElementById("bookmark-list-section");
 const viewCategory = document.getElementById("view-category-button");
 const addBookmark = document.getElementById("add-bookmark-button");
-const goBack = document.getElementById("close-form-button");
+const closeFormButton = document.getElementById("close-form-button");
 const addBookmarkForm = document.getElementById("add-bookmark-button-form");
+const categoryName = document.querySelector(".category-name");
+const categoryDropdown = document.getElementById("category-dropdown");
+const nameForm = document.getElementById("name");
+const urlForm = document.getElementById("url");
+const closeListButton = document.getElementById("close-list-button");
+
+const getBookmarks = () => {
+  const storedBookMarks = localStorage.getItem('bookmarks')
+  if(!storedBookMarks){
+    return [];
+   }
+   try {
+    const bookmarks = JSON.parse(storedBookMarks);
+    return Array.isArray(bookmarks) && (typeof(bookmarks[0]) === "object") && bookmarks[0].hasOwnProperty("name") && bookmarks[0].hasOwnProperty("category") && bookmarks[0].hasOwnProperty("url") ? bookmarks : [];
+  } catch {
+    return [];
+  }
+}
 
 
-const changeViews = () => {
-  // bookmarkList.classList.toggle("hidden");
+function updateBookmarks() {
+    var existingBookmarks = getBookmarks();
+    var entryName = nameForm.value;
+    var entryURL = urlForm.value;
+    var entry = {
+        "name": entryName,
+        "category": categoryDropdown.value,
+        "url": entryURL
+    };
+    
+    existingBookmarks.push(entry);
+    localStorage.setItem("bookmarks", JSON.stringify(existingBookmarks));
+    nameForm.value = "";
+    urlForm.value = "";
+    displayOrCloseForm();
+};
+
+const displayOrCloseForm = () => {
+  mainSection.classList.toggle("hidden");
+  formSection.classList.toggle("hidden");
+};
+
+const displayOrHideCategory = () => {
+  mainSection.classList.toggle("hidden");
+  bookmarkListSection.classList.toggle("hidden");
+}
+
+const displayOrCloseList = () => {
   mainSection.classList.toggle("hidden");
   bookmarkListSection.classList.toggle("hidden");
 };
 
-viewCategory.addEventListener("click", changeViews);
+viewCategory.addEventListener("click", () => {displayOrHideCategory()} );
+closeFormButton.addEventListener("click", displayOrCloseForm);
+addBookmark.addEventListener("click", () =>{
+  categoryName.innerText = categoryDropdown.value;
+  displayOrCloseForm()});
+addBookmarkForm.addEventListener("click", updateBookmarks)
+closeListButton.addEventListener("click", displayOrCloseList)
 
-goBack.addEventListener("click", changeViews);
 
 
