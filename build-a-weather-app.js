d0495c74654b7dac11c6537b461f13c354074322
Jules <195559278+spacecookie007@users.noreply.github.com>
2026-02-05 11:45:33 +0100

    Final solution

--- build-a-weather-app.js
+++ build-a-weather-app.js
@@ -1,23 +1,36 @@
-const getWeatherBttn = document.getElementById("get-weather");
+const getWeatherBttn = document.getElementById("get-weather-btn");
 const cities = document.getElementById("cities");
 
 
-async function getWeather(city){
-  return fetch(`https://weather-proxy.freecodecamp.rocks/api/city/${city}`)
-  .catch(error => console.error('Error:', error));
+async function getWeather(city) {
+  try {
+    const response = await fetch(`https://weather-proxy.freecodecamp.rocks/api/city/${city}`)
+    if (!response.ok)
+      throw new Error(`Weather information for city '${city}' not found.`);
+    let data = response.json();
+    return data;
+  } catch (error) {
+    console.log(error)
+    alert("Something went wrong, please try again later")
+    return 1
+  }
 }
 
+
 async function showWeather(city){
-  try {
-    let response = await getWeather(city);
-    let data = await response.json();
+    let data = await getWeather(city);
     document.getElementById("location").textContent = data.name;
     document.getElementById("weather-icon").src = data.weather[0].icon;
-
-    console.log(data);
-  } catch (error) {
-    console.log("Something went wrong, please try again later", error);
-  }
+    document.getElementById("main-temperature").textContent = `${data.main.temp} ° C`;
+    document.getElementById("feels-like").textContent = `${data.main.feels_like} ° C`;
+    document.getElementById("humidity").textContent = `Humidity: ${data.main.humidity}%`;
+    document.getElementById("wind").textContent = `Wind speed: ${data.wind.speed} m/s`;
+    if(!data.wind.gust){
+      document.getElementById("wind-gust").textContent = "N/A";
+    }else{
+      document.getElementById("wind-gust").textContent = `Gusts: ${data.wind.gust} m/s`;
+    }
+    document.getElementById("weather-main").textContent = data.weather[0].main;
 }
 
 getWeatherBttn.addEventListener("click", () => {
@@ -25,5 +38,3 @@ getWeatherBttn.addEventListener("click", () => {
     showWeather(cities.value)
   }
   })
-
-// cities.addEventListener("change", () => console.log(cities.value))
